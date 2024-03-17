fbcd579f32c535885a40b21fc1296c95f9e8c3fb
iced-penguin <icedpenguin0504@gmail.com>
2024-03-17 18:50:29 +0900

    add: 最大公約数、最小公倍数を求める関数

--- src/lib.rs
+++ src/lib.rs
@@ -41,3 +41,51 @@ fn test_join_to_string() {
     // 要素数1
     assert_eq!(join_to_string(&vec!["a"], " "), "a");
 }
+
+#[snippet]
+/// a, b の最大公約数を求める（Euclidの互除法）
+pub fn gcd<T>(a: T, b: T) -> T
+where T: num::Unsigned + Copy
+{
+    if b == T::zero() { a } else { gcd(b, a % b) }
+}
+
+#[test]
+fn test_gcd() {
+    // u8, a > b
+    assert_eq!(gcd(10u8, 5u8), 5);
+    // u16, a < b
+    assert_eq!(gcd(14u16, 21u16), 7);
+    // u32, a = b
+    assert_eq!(gcd(100u32, 100u32), 100);
+    // u64, b = 1
+    assert_eq!(gcd(5u64, 1u64), 1);
+    // u128, a = 1
+    assert_eq!(gcd(1u128, 5u128), 1);
+    // usize, 互いに素
+    assert_eq!(gcd(17usize, 23usize), 1);
+}
+
+#[snippet(include = "gcd")]
+/// a, b の最小公倍数を求める
+pub fn lcm<T>(a: T, b: T) -> T 
+where T: num::Unsigned + Copy
+{
+    a * b / gcd(a, b)
+}
+
+#[test]
+fn test_lcm() {
+    // u8, a > b
+    assert_eq!(lcm(10u8, 5u8), 10);
+    // u16, a < b
+    assert_eq!(lcm(5u16, 10u16), 10);
+    // u32, a = b
+    assert_eq!(lcm(10u32, 10u32), 10);
+    // u64, b = 1
+    assert_eq!(lcm(14u64, 1u64), 14);
+    // u128, a = 1
+    assert_eq!(lcm(1u128, 17u128), 17);
+    // usize, 互いに素
+    assert_eq!(lcm(17usize, 23usize), 17 * 23);
+}
